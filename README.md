f4e199ea1e65066901293f2a937e3ad331486c87
Peter Juritz <peter.juritz@gmail.com>
2013-03-12 14:30:39 +0200

    Added IRC code. Fixed some problems with unicode strings.

--- README.md
+++ README.md
@@ -1,4 +1,19 @@
 gumboot
 =======
 
-An IRC bot to relay gumtree RSS feeds to IRC
+An IRC bot to relay gumtree RSS feeds to IRC. An example config file is in example_config.json.
+
+Installation
+============
+Requires the python modules:
+pip install irc
+pip install feedparser
+pip install hgtools # required for irc module
+
+
+Running
+=======
+Usage: gumboot.py <config_file> <title_cache_file>
+    <config_file> : JSON encoded config dictionary
+    <title_cache_file> : A file to store md5sums of titles to avoid duplicates on restart
+
--- gumboot.py
+++ gumboot.py
@@ -2,53 +2,88 @@ import feedparser
 import hashlib
 import json
 import sys
+import irc.client
+from time import sleep, mktime
+from datetime import datetime
 
 CONFIG = {}
 CACHE_FILE = None
 CACHE = set([])
 
 def load_config(filename):
-    with open(filename) as f:
+    with open(filename,'r') as f:
         confstr = f.read()
         config = json.loads(confstr) # No validation for now
 
     global CONFIG
     CONFIG = config
-    global CACHE_FILE
 
 def hashed(s):
-    return hashlib.md5(s).hexdigest()
+    return hashlib.md5(s.encode('UTF-8')).hexdigest()
 
 def read_cache_file(filename):
-    global CACHE_FILE
-    global CACHE
-    CACHE_FILE = open(filename,'rw')
-    for l in CACHE_FILE:
-        if not l.strip():
-            continue
-        cache_entry = hashed(l.split()[0])
-        CACHE.add(cache_entry)
+    try:
+        global CACHE_FILE
+        global CACHE
+        CACHE_FILE = open(filename,'rw')
+        for l in CACHE_FILE:
+            if not l.strip():
+                continue
+            cache_entry =l.split()[0]
+            CACHE.add(cache_entry)
+    except IOError: # no cache file
+        CACHE_FILE = open(filename,'w')
+        CACHE_FILE.flush()
+        return
+    except:
+        print 'Malformed cache file'
+        sys.exit(1)
+
+    CACHE_FILE.close() # use r+ ?
+    CACHE_FILE = open(filename,'a')
+
 
 def seen_before(entry):
     return hashed(entry.title) in CACHE
 
 def add_to_cache_file(entry): # only add to cache file after successfully sent to IRC
+    global CACHE_FILE
     he = hashed(entry.title)
-    CACHE_FILE.write('%s %s\n' % (he, entry.title))
-    CACHE.add(he)
+    if he not in CACHE:
+        CACHE_FILE.write('%s %s\n' % (he, entry.title.encode('UTF-8')))
+        CACHE_FILE.flush()
+        CACHE.add(he)
 
-def dance():
+def connect_and_join(client):
+    connection = client.server().connect(CONFIG['irc_server'], CONFIG['irc_port'], CONFIG['irc_nick'])
+    chan = CONFIG['irc_channel']
+    sleep(10)
+    connection.join(chan)
+    connection.privmsg(chan, 'Hello from Gumboot')
+    return connection
+
+def dance(connection):
     rss_url = CONFIG['rss_url']
     feed = feedparser.parse(rss_url)
+    chan = CONFIG['irc_channel']
+    notifiers = ' '.join([nick for nick in CONFIG['nicks']])
 
-    for ent in  feed['entries']:
-        print ent.title, hashlib.md5(ent.title).hexdigest()
+    for ent in  feed['entries'].__reversed__():
+        if not seen_before(ent):
+            formatted_time = str(datetime.fromtimestamp(mktime(ent.updated_parsed)))
+            connection.privmsg(chan, '%s : %s %s (%s)' % (notifiers, ent.title, ent.links[0].href, formatted_time))
+            add_to_cache_file(ent)
+            sleep(0.4)
+
 if __name__ == '__main__':
-    if len(sys.argv) != 3:
+    if len(sys.argv) != 3 or '--help' in sys.argv or 'help' in sys.argv or '-h' in sys.argv:
         print 'Usage: %s <config_file> <title_cache_file>' % sys.argv[0]
         print '\t<config_file> : JSON encoded config dictionary'
         print '\t<title_cache_file> : A file to store md5sums of titles to avoid duplicates on restart'
         sys.exit(1)
     load_config(sys.argv[1])
     read_cache_file(sys.argv[2])
-    dance()
+    client = irc.client.IRC()
+    c = connect_and_join(client)
+    c.execute_every(CONFIG['poll_interval'], dance, (c,))
+    client.process_forever()
